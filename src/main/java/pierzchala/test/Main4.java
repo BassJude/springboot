def140e97abe1aee008dfead2d74e8b16a852e15
BassJude <adam.pierzchala.muzyk@gmail.com>
2019-09-15 13:05:28 +0200

    V1.1

--- src/main/java/pierzchala/test/Main4.java
+++ src/main/java/pierzchala/test/Main4.java
@@ -1,10 +1,20 @@
 package pierzchala.test;
 
-import org.springframework.core.env.Environment;
+import java.util.Scanner;
 
 public class Main4 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int number=0;
 
+        System.out.println("Podaj liczbę: ");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Podaj liczbę");
+
+        }
+        number = scanner.nextInt();
+        System.out.println("Wpisałeś: "+number);
     }
 }
